1504c022d2ccc7c3d7a3a9bcb21c65911497ce6b
M-Yankov <pointblank12345@abv.bg>
2016-02-15 16:21:23 +0200

    About page

---
+++ UniversityStudentSystem/Web/UniversityStudentSystem.Web/Areas/Public/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿namespace UniversityStudentSystem.Web.Areas.Public.Controllers
+{
+    using System.Web.Mvc;
+
+    public class AboutController : Controller
+    {
+        // GET: Public/About
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
