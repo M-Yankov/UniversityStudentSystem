32e2d5c9c8a83832177e807db983004f7ee140be
M-Yankov <pointblank12345@abv.bg>
2016-02-22 18:30:36 +0200

    Users can see marks in the profile.

--- UniversityStudentSystem/Web/UniversityStudentSystem.Web/Models/Marks/MarkViewModel.cs
+++ UniversityStudentSystem/Web/UniversityStudentSystem.Web/Models/Marks/MarkViewModel.cs
@@ -1,11 +1,14 @@
 ﻿namespace UniversityStudentSystem.Web.Models.Marks
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using AutoMapper;
     using Data.Models;
     using UniversityStudentSystem.Web.Infrastructure.Mapping;
     using Users;
+    using System.Linq;
 
     public class MarkViewModel : IMapFrom<Mark>, IHaveCustomMappings
     {
@@ -19,10 +22,18 @@
 
         public int CourseId { get; set; }
 
+        public string Course { get; set; }
+
+        public IList<string> Trainers { get; set; }
+
         public void CreateMappings(IMapperConfiguration configuration)
         {
             configuration.CreateMap<Mark, MarkViewModel>()
-                .ForMember(c => c.Username, o => o.MapFrom(m => m.User.UserName));
+                .ForMember(c => c.Username, o => o.MapFrom(m => m.User.UserName))
+                .ForMember(c => c.Course, o => o.MapFrom(m => m.Course.Name))
+                .ForMember(
+                    c => c.Trainers,
+                    o => o.MapFrom(m => m.Course.Trainers.Select(t => t.FirstName + " " + t.LastName)));
         }
     }
 }
