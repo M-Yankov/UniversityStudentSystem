02a8b583e7ffa012eaf9e6098f470bc03d435267
M-Yankov <pointblank12345@abv.bg>
2017-05-21 14:41:17 +0300

    #18 Added forums relation to category model.

--- UniversityStudentSystem/Data/UniversityStudentSystem.Data.Models/Category.cs
+++ UniversityStudentSystem/Data/UniversityStudentSystem.Data.Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿namespace UniversityStudentSystem.Data.Models
 {
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using Common;
@@ -7,10 +8,30 @@
 
     public class Category : BaseModel<int>
     {
+        private ICollection<ForumPost> forumPosts;
+
+        public Category()
+        {
+            this.forumPosts = new HashSet<ForumPost>();
+        }
+
         [Required]
         [Index(IsUnique = true)]
         [MinLength(ModelConstants.NameMinLength)]
         [MaxLength(ModelConstants.NameMaxLength)]
         public string Name { get; set; }
+
+        public virtual ICollection<ForumPost> ForumPosts
+        {
+            get
+            {
+                return forumPosts;
+            }
+
+            set
+            {
+                forumPosts = value;
+            }
+        }
     }
 }
