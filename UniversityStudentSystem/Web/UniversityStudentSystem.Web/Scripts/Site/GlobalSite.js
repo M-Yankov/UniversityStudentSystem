f7dbef07a04ea1e803719253545b09b7a6a88cff
M-Yankov <pointblank12345@abv.bg>
2017-05-21 13:47:48 +0300

    Configurations for fonts in web.config. Changes in UI, disabled body scrolling when nav menu is expanded on mobile. #33

---
+++ UniversityStudentSystem/Web/UniversityStudentSystem.Web/Scripts/Site/GlobalSite.js
@@ -0,0 +1,17 @@
+﻿(function ($) {
+
+    // http://getbootstrap.com/javascript/#collapse - scroll down to events
+
+    $(document.body).on('hide.bs.collapse', '.navbar-collapse', function (e) {
+        if ($(e.target).is('div.sidebar-nav.navbar-collapse.collapse')) {
+            $(document.body).css('overflow', ''); // enables scrolling
+        }
+    })
+
+    $(document.body).on('show.bs.collapse', '.navbar-collapse', function (e) {
+        if ($(e.target).is('div.sidebar-nav.navbar-collapse.collapse')) {
+            $(document.body).css('overflow', 'hidden'); // disables scrolling
+        }
+    })
+
+})(jQuery)
